4b71a67bf43d490d4979f4879c98acb67a36fc77
Yuhao L <36289795+Way-Yuhao@users.noreply.github.com>
2022-06-27 20:37:30 -0500

    working with dataframes

--- tmp/test2.py
+++ tmp/test2.py
@@ -1,10 +1,14 @@
 import numpy as np
+import pandas as pd
 
 
 def main():
     a = np.array([1, 2, 3, 0, 0])
+    b = np.array([3, 4, 3, 2, 2])
     print(np.average(a[a != 0]))
-
-
+    d = {'col1': a, 'col2': b}
+    df = pd.DataFrame(data=d)
+    print(df)
+    df.to_csv
 if __name__ == '__main__':
     main()
