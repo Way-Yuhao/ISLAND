4de6aa31b1230906a0f64d4d2a92ae8683a928c6
Yuhao Liu <yuhao.liu@rice.edu>
2022-08-24 11:29:51 -0500

    multiprocess working

--- batch_eval.py
+++ batch_eval.py
@@ -12,7 +12,7 @@ from natsort import natsorted
 from tqdm import tqdm
 import random
 from config import *
-from util.helper import get_season
+from util.helper import get_season, rprint, yprint, time_func
 
 
 def evaluate():
@@ -172,53 +172,51 @@ def plot_temporal_pairwise():
     plt.show()
 
 
-def temp_pairwise_cycle_eval(city_name):
+def temp_pairwise_cycle_eval_single(root_, target_date, ref_frame, log):
+    interp = Interpolator(root=root_, target_date=target_date)
+    interp.occluded_target = interp.target  # assume no cloud
+
+    ref_date = ref_frame[9:17]
+    ref_perc = interp.temporal_interp(ref_frame_date=ref_date)
+
+    mae_loss = interp.calc_loss(print_=True, metric='mae', entire_canvas=True)
+    mse_loss = interp.calc_loss(print_=True, metric='mse', entire_canvas=True)
+    log += [(target_date, ref_date, np.nan, np.nan, ref_perc, mae_loss, mse_loss)]
+    interp.save_output()
+    del interp
+
+
+@time_func
+def temp_pairwise_cycle_eval_mp(city_name):
     """
     fixed cloud-free target, fixed synthetic occlusion, using a set of real reference frames (potentially cloudy)
     :param city_name:
     :return:
     """
-    # entries = pd.read_csv(f"./data/{city_name}/rand_dates.csv")
-    target_date = '20220611'
-    # ref_date = '20181205'
+    target_date = '20220424'
+    # vars for multiprocess
+    num_procs = 10
+    pool = Pool(num_procs)
+    manager = Manager()
+    lock = manager.Lock()
+    r = None  # return values
     # FIXME: no synthetic cloud?
     root_ = f'./data/{city_name}/'
     log_fpath = f"./data/{city_name}/temporal_references.csv"
     assert not p.exists(log_fpath)
-    log = []
+    log = manager.list()
     frames = os.listdir(p.join(root_, 'bt_series'))
     frames = [f for f in frames if 'tif' in f]
-    pbar = tqdm(total=len(frames), desc='Analyzing multiple references')
     for ref_frame in frames:
-        interp = Interpolator(root=root_, target_date=target_date)
-        interp.occluded_target = interp.target  # assume no cloud
-
-        ref_date = ref_frame[9:17]
-        ref_perc = interp.temporal_interp(ref_frame_date=ref_date)
-
-        mae_loss = interp.calc_loss(print_=True, metric='mae', entire_canvas=True)
-        mse_loss = interp.calc_loss(print_=True, metric='mse', entire_canvas=True)
-        log += [(target_date, ref_date, np.nan, np.nan, ref_perc, mae_loss, mse_loss)]
-        interp.save_output()
-        del interp
-        pbar.update()
-
-        # target_syn_cloud_date, ref_syn_cloud_date = row['date_1'], row['date_2']
-        # interp = Interpolator(root=root_, target_date=target_date)
-        # ref_syn_cloud_path = p.join(root_, 'cloud', f'LC08_cloud_houston_{target_syn_cloud_date}.tif')
-        # target_perc = interp.add_occlusion(ref_syn_cloud_path)
-        # ref_perc = interp.temporal_interp_cloud(ref_frame_date=ref_date, ref_syn_cloud_date=ref_syn_cloud_date)
-        # mae_loss = interp.calc_loss(print_=True, metric='mae', entire_canvas=True)
-        # mse_loss = interp.calc_loss(print_=True, metric='mse', entire_canvas=True)
-        # log += [(target_date, ref_date, target_syn_cloud_date, target_perc, ref_syn_cloud_date, ref_perc,
-        #          mae_loss, mse_loss)]
-        # interp.save_output()
-        # del interp
-        # pbar.update()
-    pbar.close()
-    df = pd.DataFrame(log, columns=['target_date', 'ref_date', 'target_syn_cloud_date',
-                                    'target_synthetic_occlusion_percentage',
-                                    'reference_gt_occlusion_percentage', 'MAE', 'MSE'])
+        r = pool.apply_async(temp_pairwise_cycle_eval_single, args=(root_, target_date, ref_frame, log))
+    r.get()
+    pool.close()
+    pool.join()
+    print('---------------------------------')
+    yprint('Multi-process pool finished. Scroll up to view potential errors')
+    df = pd.DataFrame(list(log), columns=['target_date', 'ref_date', 'target_syn_cloud_date',
+                                          'target_synthetic_occlusion_percentage',
+                                          'reference_gt_occlusion_percentage', 'MAE', 'MSE'])
     df.to_csv(log_fpath, index=False)
     print('csv file saved to ', log_fpath)
 
@@ -235,12 +233,12 @@ def plot_temporal_cycle(city_name):
     y = df['MAE']
     sns.set_style('darkgrid')
     plt.figure(num=1, figsize=(8, 5))
-    # g = sns.jointplot(x=x, y=y, c=z, joint_kws={"color": None, 'cmap': 'cool'})
-    # g.fig.colorbar(g.ax_joint.collections[0], ax=[g.ax_joint, g.ax_marg_y, g.ax_marg_x], use_gridspec=True,
-    #                orientation='horizontal', label=f'MAE loss, clipped at {max_clip}')
-    # g.set_axis_labels('occlusion percentage of the reference frame', 'occlusion percentage of the target frame', )
     sns.scatterplot(x=ref_dates, y=y, hue=seasons)
     sns.lineplot(x=ref_dates, y=df['reference_gt_occlusion_percentage'], color='black')
+
+    plt.title('Choice of reference frame vs. MAE loss')
+    plt.xlabel('Date corresponding to reference frame')
+    plt.ylabel('MAE loss, temporal channel only')
     plt.show()
 
 
@@ -255,5 +253,5 @@ if __name__ == '__main__':
     # generate_dps(city_name='Phoenix')
     # temp_pairwise_eval(city_name='Phoenix')
     # plot_temporal_pairwise()
-    temp_pairwise_cycle_eval(city_name='Phoenix')
-    # plot_temporal_cycle(city_name='Phoenix')
+    # temp_pairwise_cycle_eval_mp(city_name='Phoenix')
+    plot_temporal_cycle(city_name='Phoenix')
