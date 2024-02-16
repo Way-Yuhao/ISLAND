8f34bc68c87197f215fe6280fc97e9e89e1c7dea
Way-Yuhao <36289795+Way-Yuhao@users.noreply.github.com>
2024-02-16 16:22:40 -0600

    downloading lst data

---
+++ tmp/test.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+def send_slack_message(webhook_url, message):
+    data = {'text': message}
+    response = requests.post(webhook_url, json=data)
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+
+
+if __name__ == '__main__':
+    # webhook_url = 'https://hooks.slack.com/services/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'
+    # message = 'Hello, world!'
+    # send_slack_message(webhook_url, message)
+    webhook_url = os.getenv('SLACK_WEBHOOK_URL')
+    print(webhook_url)
+
