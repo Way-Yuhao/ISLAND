61ba57001664b64194112302b5e197cd7094bdd3
Way-Yuhao <36289795+Way-Yuhao@users.noreply.github.com>
2024-02-20 17:25:12 -0600

    our lst vs landsat lst

--- scripts/sample_cities.sh
+++ scripts/sample_cities.sh
@@ -1,43 +1,43 @@
 # cd ..;
-python main.py -c New York --skip_to_ref --no_wandb;
-python main.py -c Los Angeles --skip_to_ref --no_wandb;
-python main.py -c Chicago --skip_to_ref --no_wandb;
-python main.py -c Houston --skip_to_ref --no_wandb;
-python main.py -c Phoenix --skip_to_ref --no_wandb;
-python main.py -c Philadelphia --skip_to_ref --no_wandb;
-python main.py -c San Antonio --skip_to_ref --no_wandb;
-python main.py -c San Diego --skip_to_ref --no_wandb;
-python main.py -c Dallas --skip_to_ref --no_wandb;
-python main.py -c San Jose --skip_to_ref --no_wandb;
-python main.py -c Austin --skip_to_ref --no_wandb;
-python main.py -c Jacksonville --skip_to_ref --no_wandb;
-python main.py -c Fort Worth --skip_to_ref --no_wandb;
-python main.py -c Columbus --skip_to_ref --no_wandb;
-python main.py -c Indianapolis --skip_to_ref --no_wandb;
-python main.py -c Charlotte --skip_to_ref --no_wandb;
-python main.py -c San Francisco --skip_to_ref --no_wandb;
-python main.py -c Seattle --skip_to_ref --no_wandb;
-python main.py -c Denver --skip_to_ref --no_wandb;
-python main.py -c Oklahoma City --skip_to_ref --no_wandb;
+#python main.py -c New York --skip_to_ref --no_wandb;
+#python main.py -c Los Angeles --skip_to_ref --no_wandb;
+#python main.py -c Chicago --skip_to_ref --no_wandb;
+#python main.py -c Houston --skip_to_ref --no_wandb;
+#python main.py -c Phoenix --skip_to_ref --no_wandb;
+#python main.py -c Philadelphia --skip_to_ref --no_wandb;
+#python main.py -c San Antonio --skip_to_ref --no_wandb;
+#python main.py -c San Diego --skip_to_ref --no_wandb;
+#python main.py -c Dallas --skip_to_ref --no_wandb;
+#python main.py -c San Jose --skip_to_ref --no_wandb;
+#python main.py -c Austin --skip_to_ref --no_wandb;
+#python main.py -c Jacksonville --skip_to_ref --no_wandb;
+#python main.py -c Fort Worth --skip_to_ref --no_wandb;
+#python main.py -c Columbus --skip_to_ref --no_wandb;
+#python main.py -c Indianapolis --skip_to_ref --no_wandb;
+#python main.py -c Charlotte --skip_to_ref --no_wandb;
+#python main.py -c San Francisco --skip_to_ref --no_wandb;
+#python main.py -c Seattle --skip_to_ref --no_wandb;
+#python main.py -c Denver --skip_to_ref --no_wandb;
+#python main.py -c Oklahoma City --skip_to_ref --no_wandb;
 
 
-#python region_sampler.py -c New York
-#python region_sampler.py -c Los Angeles
-#python region_sampler.py -c Chicago
-#python region_sampler.py -c Houston
-#python region_sampler.py -c Phoenix
-#python region_sampler.py -c Philadelphia
-#python region_sampler.py -c San Antonio
-#python region_sampler.py -c San Diego
-#python region_sampler.py -c Dallas
-#python region_sampler.py -c San Jose
-##python region_sampler.py -c Austin
-#python region_sampler.py -c Jacksonville
-#python region_sampler.py -c Columbus
-#python region_sampler.py -c Indianapolis
-#python region_sampler.py -c Charlotte
-#python region_sampler.py -c San Francisco
-#python region_sampler.py -c Seattle
-#python region_sampler.py -c Denver
-#python region_sampler.py -c Oklahoma City
+python region_sampler.py -c New York
+python region_sampler.py -c Los Angeles
+python region_sampler.py -c Chicago
+python region_sampler.py -c Houston
+python region_sampler.py -c Phoenix
+python region_sampler.py -c Philadelphia
+python region_sampler.py -c San Antonio
+python region_sampler.py -c San Diego
+python region_sampler.py -c Dallas
+python region_sampler.py -c San Jose
+#python region_sampler.py -c Austin
+python region_sampler.py -c Jacksonville
+python region_sampler.py -c Columbus
+python region_sampler.py -c Indianapolis
+python region_sampler.py -c Charlotte
+python region_sampler.py -c San Francisco
+python region_sampler.py -c Seattle
+python region_sampler.py -c Denver
+python region_sampler.py -c Oklahoma City
 
