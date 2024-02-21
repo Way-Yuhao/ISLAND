43d70491b9479b20541f138ebf65bad122efe93b
Way-Yuhao <36289795+Way-Yuhao@users.noreply.github.com>
2024-02-20 22:07:33 -0600

    there is size error for Phoenix, LA, and Chicago

--- scripts/sample_cities.sh
+++ scripts/sample_cities.sh
@@ -21,23 +21,23 @@
 #python main.py -c Oklahoma City --skip_to_ref --no_wandb;
 
 
-python region_sampler.py -c New York
+#python region_sampler.py -c New York
 python region_sampler.py -c Los Angeles
 python region_sampler.py -c Chicago
-python region_sampler.py -c Houston
+#python region_sampler.py -c Houston
 python region_sampler.py -c Phoenix
-python region_sampler.py -c Philadelphia
-python region_sampler.py -c San Antonio
-python region_sampler.py -c San Diego
-python region_sampler.py -c Dallas
-python region_sampler.py -c San Jose
+#python region_sampler.py -c Philadelphia
+#python region_sampler.py -c San Antonio
+#python region_sampler.py -c San Diego
+#python region_sampler.py -c Dallas
+#python region_sampler.py -c San Jose
 #python region_sampler.py -c Austin
-python region_sampler.py -c Jacksonville
-python region_sampler.py -c Columbus
-python region_sampler.py -c Indianapolis
-python region_sampler.py -c Charlotte
-python region_sampler.py -c San Francisco
-python region_sampler.py -c Seattle
-python region_sampler.py -c Denver
-python region_sampler.py -c Oklahoma City
+#python region_sampler.py -c Jacksonville
+#python region_sampler.py -c Columbus
+#python region_sampler.py -c Indianapolis
+#python region_sampler.py -c Charlotte
+#python region_sampler.py -c San Francisco
+#python region_sampler.py -c Seattle
+#python region_sampler.py -c Denver
+#python region_sampler.py -c Oklahoma City
 
